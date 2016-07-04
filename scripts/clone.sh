7a13efe35aeb396f1a06517339d7d5bc21ce4f4d
chenxiaodong <cxdongjack@163.com>
2016-07-04 22:52:38 +0800

    fix: only clone depth 7

--- scripts/clone.sh
+++ scripts/clone.sh
@@ -9,7 +9,7 @@ printf "\n[-] clone application...\n\n"
 
 # Customize packages
 cd ~
-git clone $APP_GIT $APP_SOURCE_DIR
+git clone --depth 7 $APP_GIT $APP_SOURCE_DIR
 cd $APP_SOURCE_DIR
 git submodule init
 git submodule update
